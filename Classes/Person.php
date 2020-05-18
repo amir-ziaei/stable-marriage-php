38a604c8a3813880c85b2cdc47689b7631fbf96b
k1lleR8 <amirziaee12@gmail.com>
2020-05-18 18:29:21 +0300

    Extra attributes were removed

--- Classes/Person.php
+++ Classes/Person.php
@@ -2,22 +2,18 @@
 
 Class Person
 {
-    static $index = 0;
     public $key;
     public $preferences = [];
     public $match = NULL;
-    public $id;
 
     public function __construct($key)
     {
         $this->key = $key;
-        $this->id = ++self::$index;
     }
 
     public function add_preference($person)
     {
-        if($this->id !== $person->id)
-            $this->preferences[] = $person;
+        $this->preferences[] = $person;
     }
 
     public function is_single()
